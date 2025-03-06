af53476654793a772a37bff15fc398d8d40c136a
Shyam <gshyamprasad2001@gmail.com>
2025-03-06 16:20:33 +0530

    feat:Mutations added

--- src/components/Events/EventForm.jsx
+++ src/components/Events/EventForm.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react';
-
+import { useQuery } from '@tanstack/react-query';
 import ImagePicker from '../ImagePicker.jsx';
-
+import { fetchSelectableImages } from '../../util/http.js';
 export default function EventForm({ inputData, onSubmit, children }) {
   const [selectedImage, setSelectedImage] = useState(inputData?.image);
+
+
+   const {data,isPending,isError,error} =useQuery({
+    queryKey:['events-images'],
+    queryFn:fetchSelectableImages,
+   })
 
   function handleSelectImage(image) {
     setSelectedImage(image);
@@ -29,14 +35,23 @@ export default function EventForm({ inputData, onSubmit, children }) {
           defaultValue={inputData?.title ?? ''}
         />
       </p>
+    {isPending && <p>Loading Sek=lectable images ..
+    </p>}
 
-      <div className="control">
+    {isError && <p>Failed to load selectable images: {error.info?.message || "An error occurred"}</p>}
+
+
+     { data && (<div className="control">
         <ImagePicker
-          images={[]}
+          images={data}
           onSelect={handleSelectImage}
           selectedImage={selectedImage}
+
         />
       </div>
+     )
+     }
+     
 
       <p className="control">
         <label htmlFor="description">Description</label>
